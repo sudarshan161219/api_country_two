cde61f29d6099a9893edc0b2397137875518ee3e
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-10-26 20:09:12 +0530

    update

--- src/Info.js
+++ src/Info.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom';
+import { BsArrowLeft } from 'react-icons/bs'
+
 
 const Info = () => {
 
@@ -21,7 +23,11 @@ const fetchInfo = async () => {
     fetchInfo()
     }, [id])
 
-console.log(info)
+    const prepareCurrencies = (currency) => {
+      if (!currency) return;
+      return Object.values(currency).map((curr) => curr.name);
+    };
+
 
   return (
   <>
@@ -31,22 +37,68 @@ console.log(info)
     <article className='info-box' >
 {info.map((item, index) => {
   return(
-    <>  
+    // <>  
     <div  key={item}>
-  <div className='btn-img'>
-    <div>
-        <Link to={'/'} style={{ textDecoration: 'none' }} > <button> back </button></Link>
+
+<div className='btn-container' >
+<Link to={'/'} style={{ textDecoration: 'none', width:'200px' }} > 
+<button className='info-btn'> <BsArrowLeft className='btn-arrow' /> back </button>
+</Link>
+</div>
+
+<div className='img-container'>
+<img className="info-img" alt={item.name.official} src={item.flags.png}  />
+</div>
+
+
+
+<div className='country-info-box'>
+    <h1 className='country-info-heading'>{item.name.official}</h1>
+    <ul className='country-info-list'>
+
+      <li className='country-info-list-items'>
+        Native Name: <span>{Object.values(item.name.nativeName)[0]?.common}</span>
+      </li>
+
+      <li className='country-info-list-items'>
+        Population: <span>{item.population}</span>
+      </li>
+
+      <li className='country-info-list-items'>
+        Region: <span>{item.region}</span> 
+      </li>
+
+      <li className='country-info-list-items'>
+        Sub Region: <span>{item.subregion}</span>
+      </li>
+
+      <li className='country-info-list-items'>
+        Capital: <span>{item.capital}</span>
+      </li>
+
+    </ul>
+
+    <ul className='country-info-list-two'>
+      
+      <li className='country-info-list-items'>
+        Top Level Domain: <span>{item.tld}</span>
+      </li>
+
+      <li className='country-info-list-items'>
+        Currencies: <span>{prepareCurrencies(item.currencies)}</span>
+      </li>
+
+      <li className='country-info-list-items'>
+        Languages: <span>{item.region}</span> 
+      </li>
+
+    </ul>
+</div>
+
+
+
     </div>
-
-  <img alt={item.name.official} src={item.flags.png}  />
-      </div>
-
-
-
-
-  <h1>{item.name.official}</h1>
-    </div>
-    </>
+    // </>
   )
 
 })}
