f1c2c736ddcdb8b916a125d79d8013789ef82ae0
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-11-01 12:05:24 +0530

    update

--- src/Header.js
+++ src/Header.js
@@ -6,13 +6,15 @@ const Header = (props) => {
  
 
     return (
-      <nav className='header-nav'>        
+      <header>
+      <nav>        
       <h1>Where in the world?</h1>
       <div onClick={props.toggle} className='dark-light'>
      {props.theme === 'dark' ? <HiOutlineSun  className='icon' /> : <HiOutlineMoon  className='icon' /> }   
       <p>{props.theme === 'dark' ? 'Light Mode': 'Dark Mode'}</p>  
       </div>
       </nav>
+      </header>
     );
   };
   
