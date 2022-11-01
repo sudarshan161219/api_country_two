790175e43c9031ff5dc91aa14bcfcf3baba1da75
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-11-01 11:53:01 +0530

    update

--- src/Main.js
+++ src/Main.js
@@ -4,24 +4,12 @@ import { AiOutlineSearch} from 'react-icons/ai'
 
 const Main = (props) => {
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-const value = props.input.current.value
-if(value === "")return
-props.country(prev => {
-  return[...prev, value]
-})
-props.input.current.value =""
-    }
 
     return (
 
-        <section className='main-div'>
-        <h1 className='section-heading'>hello</h1>
-        <form className='form' onSubmit={handleSubmit}>
-        <div className='btn'>
+        <article className='main-div'>
+        <section className='form'>
         <AiOutlineSearch className='search-icon' />
-        </div>
         <input 
         placeholder='Search for a country...'
         type='search' 
@@ -30,8 +18,8 @@ props.input.current.value =""
         ref={props.inputR}
         onChange={(e) => props.country(e.target.value)}
         />
-      </form>
       </section>
+      </article>
 
     );
   };
