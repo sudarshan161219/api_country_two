effe2143999711387d611cc99f82552fa377daa0
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-11-01 14:01:26 +0530

    update

--- src/Loading.js
+++ src/Loading.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Loading = () => {
   return (
 <div className='spinner-box'>
-  {/* <div className='three-quarter-spinner'></div> */}
   <span className="loader"></span>
 </div>
   );
